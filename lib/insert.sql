ab35b4a5ced31f4d1bf31e7a6ec2f10eefb7f2a4
mrSam <samakpan817@gmail.com>
2024-02-26 16:26:04 +0100

    Created a table; bear and write querries

---
+++ lib/insert.sql
@@ -0,0 +1,28 @@
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 5, 'M', 'Blue', 'Adaptability', 'TRUE');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 15, 'F', 'Black', 'Adaptability', 'TRUE');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 21, 'M', 'Green', 'Activity', 'FALSE');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 1, 'F', 'Red', 'Persistence', 'FALSE');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 20, 'M', 'Indigo', 'Mood', 'TRUE');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 10, 'M', 'White', 'Phlegmatic', 'TRUE');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 10, 'M', 'White', 'Regularity', 'TRUE');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 5, 'M', 'White', 'Regularity', 'TRUE');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 6, 'F', 'Pink', 'Intensity', 'FALSE');
+
--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,21 @@
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+   SELECT bear.name, bear.age FROM bear WHERE sex = 'F';
 """
 
+
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+    SELECT * FROM bear ORDER BY name ASC;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+   SELECT bear.name, bear.age FROM bear WHERE alive = 'TRUE' ORDER BY age ASC;
 """
 
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT bear.name, bear.age FROM bear ORDER BY age DESC LIMIT 1;
 """
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+   SELECT bear.name, bear.age FROM bear ORDER BY age ASC LIMIT 1;
 """
+
+
